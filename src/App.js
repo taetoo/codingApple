54f9c2a792ae05d8c55dd2f2d6b2ffb7199bf3c5
taehyeonkim <rlaougs5@naver.com>
2022-09-16 11:18:34 +0900

    add map & homework

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ import { useState } from 'react';
 
 // 메인페이지
 function App() {
-  
+
   // 변수 넣을 때는 {} 로 꽂아 넣기
   let post = '인큐버스';
 
   // let [state 안에 담긴 변수, state 변경을 도와주는 함수]
   // 변수와 다르게 state의 값이 바뀌었을 때 자동으로 재렌더링이 된다.
-  let [글제목, 글제목변경] = useState(['구로동맛집','문래동맛집','대림동맛집']);
+  let [글제목, 글제목변경] = useState(['구로동맛집', '문래동맛집', '대림동맛집']);
 
-  let [좋아요, 좋아요변경] = useState(0);
+  let [좋아요, 좋아요변경] = useState([0,0,0]);
 
   let [modal, setModal] = useState(false);
 
@@ -23,7 +23,7 @@ function App() {
   let [날짜, c] = useState('9월 15일 발행');
 
   return (
-    
+
     // 클래스를 넣을 때는 className
     <div className="App">
       <div className="black-nav">
@@ -43,35 +43,52 @@ function App() {
         글제목변경(copy2); 
       }}>가나다 정렬</button> */}
 
-      <div className='list'>
-        <h4>{글제목[0]} <span onClick={()=>{좋아요변경(좋아요+1)}}>👍</span> {좋아요} </h4>
+      {/* <div className='list'>
+        <h4>{글제목[0]} <span onClick={() => { 좋아요변경(좋아요 + 1) }}>👍</span> {좋아요} </h4>
         <p>{날짜}</p>
       </div>
       <div className='list'>
-        <h4>{글제목[1]}</h4>
+        <h4>{글제목[1]}  <span onClick={() => { 좋아요변경1(좋아요1 + 1) }}>👍</span> {좋아요1} </h4>
         <p>{날짜}</p>
-      </div>
-      <div className='list'>
-        {/* 모달창 open/close  */}
-        <h4 onClick={()=>{
+      </div> */}
+
+      {/* 모달창 open/close  */}
+      {/* <div className='list'>
+      <h4 onClick={() => {
           modal == true ? setModal(false) : setModal(true);
         }}>{글제목[2]}</h4>
         <p>{날짜}</p>
-      </div>
+      </div> */}
+
+      {
+        글제목.map((a, i) => {
+          return (
+            <div className='list' key={i}>
+              <h4>{글제목[i]}
+                <span onClick={() => {
+                  let copy = [...좋아요];
+                  copy[i]++;
+                  좋아요변경(copy)
+                }}>👍</span> {좋아요[i]} </h4>
+              <p>{날짜}</p>
+            </div>
+          )
+        })
+      }
       {/* 모달창 초기에 안나오게 */}
       {
-        modal == true ? <Modal/> : null
+        modal == true ? <Modal /> : null
       }
     </div>
   );
 }
-function Modal(){
-  return(
+function Modal() {
+  return (
     <div className='modal'>
-    <h4>제목</h4>
-    <p>날짜</p>
-    <p>상세내용</p>
-  </div>
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
   )
 }
 
