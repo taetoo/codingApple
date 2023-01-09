49709378ae933da1411c2343b0fd4ac74e52b1f2
taetoo <rlaougs5@naver.com>
2023-01-09 15:17:36 +0900

    Add: props 심화

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ function App() {
   let [글제목, 글제목변경] = useState(['남자 코트 추천','강남 우동 맛집','파이썬독학']);
   let [따봉, 따봉변경] = useState([0,0,0]);
   let [modal, setModal] = useState(false);
+  let [title, setTitle] = useState(0);
+  let [입력값, 입력값변경] = useState('');
 
 
   
@@ -25,46 +27,56 @@ function App() {
         {
           글제목.map(function(a, i){
             return ( 
-              <div className="list">
-                <h4>{글제목[i]} <span onClick={()=>{
+              <div className="list" key={i}>
+                <h4 onClick={()=>{setModal(!modal); setTitle(i)}}>{글제목[i]} <span onClick={(e)=>{
+                  // 이벤트 버블링 막아주는 함수
+                  e.stopPropagation();
                   let copy = [...따봉];
                   copy[i] == 0 ? copy[i]++ : copy[i]--;
-                  따봉변경(copy)
+                  따봉변경(copy);
                 }}>👍</span> {따봉[i]}</h4>
                 <p>2월 17일 발행</p>
+                <button className="delBtn" onClick={(i)=>{
+                  let copy = [...글제목]
+                  copy.splice(copy[i])
+                  글제목변경(copy)
+                  console.log(copy)
+                }}>삭제</button>
               </div>
             )
           })
-
         }
-      
-     
-
-
+        {/* 여러 이벤트핸들러가 존재하기 때문에 필요시 찾아서 사용. 약 30개 정도 */}
+        <input onChange={(e)=>{입력값변경(e.target.value)}}></input>
+        <button onClick={()=>{
+          let copy = [...글제목]
+          copy.unshift(입력값);
+          글제목변경(copy)
+          }}>입력</button>
 
 
 
         {/* 초기 모달의 값이 false 니깐 버튼을 눌렀을 때 반대인 true 로 값이 바뀌고,
         모달 값이 true로 바뀌었으니깐 다시 버튼을 누르면 false 로 바뀐다.*/}
-        <button onClick={()=>{setModal(!modal)}}>Modal</button>
         {
-          modal == true ? <Modal 모달제목={글제목[0]}/> : null
+          modal == true ? <Modal title={title} 글제목={글제목} 글제목변경={글제목변경}/> : null
         }
     </div>
     
     
   );
 }
+  function Modal(props){
+    return(
+      <div className="modal">
+        <h4>{props.글제목[props.title]}</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+        <button>글수정</button>
+      </div>
+    )
+  }
 
-function Modal(props){
-  return(
-    <div className="modal">
-      <h4>{props.모달제목}</h4>
-      <p>날짜</p>
-      <p>상세내용</p>
-    </div>
-  )
-}
 
 
 export default App;
