1ba3c5f31aa9bcb3a1ba0163570acc5e84b84435
taetoo <rlaougs5@naver.com>
2023-01-10 11:01:01 +0900

    Fix: 인풋막기 및 좋아요 에러 해결

--- src/App.js
+++ src/App.js
@@ -23,6 +23,9 @@ function App() {
         <div className="black-nav">
           <h4>ReactBlog</h4>
         </div>
+        {
+          modal == true ? <Modal title={title} 글제목={글제목} 글제목변경={글제목변경}/> : null
+        }
         {/* state가 array 자료이면 복사부터 하고 진행! */}
         {
           글제목.map(function(a, i){
@@ -36,31 +39,38 @@ function App() {
                   따봉변경(copy);
                 }}>👍</span> {따봉[i]}</h4>
                 <p>2월 17일 발행</p>
-                <button className="delBtn" onClick={(i)=>{
-                  let copy = [...글제목]
-                  copy.splice(copy[i])
+                <button className="delBtn" onClick={()=>{
+                  let copy = [...글제목];
+                  copy.splice(i, 1)
                   글제목변경(copy)
-                  console.log(copy)
                 }}>삭제</button>
               </div>
+    
             )
           })
         }
         {/* 여러 이벤트핸들러가 존재하기 때문에 필요시 찾아서 사용. 약 30개 정도 */}
-        <input onChange={(e)=>{입력값변경(e.target.value)}}></input>
+        <input type="text" onChange={(e)=>{입력값변경(e.target.value)}}></input>
         <button onClick={()=>{
+          if(!입력값){
+            alert('입력해주세요!')
+          }else{
           let copy = [...글제목]
+          let copy2 = [...따봉]
           copy.unshift(입력값);
+          copy2.unshift(0)
           글제목변경(copy)
-          }}>입력</button>
-
+          따봉변경(copy2)
+          console.log(copy2)
+          }}}>입력</button>
+          
 
 
         {/* 초기 모달의 값이 false 니깐 버튼을 눌렀을 때 반대인 true 로 값이 바뀌고,
         모달 값이 true로 바뀌었으니깐 다시 버튼을 누르면 false 로 바뀐다.*/}
-        {
+        {/* {
           modal == true ? <Modal title={title} 글제목={글제목} 글제목변경={글제목변경}/> : null
-        }
+        } */}
     </div>
     
     
